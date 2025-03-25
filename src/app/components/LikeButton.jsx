c72d615fab0bb5f1e9c94b276bdabad0962dfc16
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-03-25 11:43:59 +0100

    like button

---
+++ src/app/components/LikeButton.jsx
@@ -0,0 +1,16 @@
+"use client";
+import { FaRegThumbsUp } from "react-icons/fa6";
+import { useState } from "react";
+
+const LikeButton = (props) => {
+  const [likes, setLikes] = useState(0);
+  return (
+    <button onClick={() => setLikes(likes + 1)}>
+      <FaRegThumbsUp />
+      {props.label}
+      {likes > 1 ? "s" : ""} {likes}
+    </button>
+  );
+};
+
+export default LikeButton;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import LikeButton from "./components/Likebutton";
 
 export default function Home() {
-  return <div>Indhold</div>;
+  return (
+    <div>
+      <LikeButton label="like" />
+      <LikeButton label="Dislike" />
+    </div>
+  );
 }
